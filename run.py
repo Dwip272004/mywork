dafe2bce167bc7d5ef011d633c61e22f5d4b928f
Dwip Lahare <123765492+Dwip272004@users.noreply.github.com>
2024-09-29 00:42:47 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import app, db
-# app.py
-app.secret_key = 'your_secret_key_here'  # Replace with a secure key
+import os
 
+# Set the secret key for session management
+app.secret_key = 'your_secret_key_here'  # Replace with a secure key
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+        db.create_all()  # Create database tables if they don't exist
+    # Get the PORT environment variable or use 5000 as a default
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port, debug=False)  # Bind to all interfaces
